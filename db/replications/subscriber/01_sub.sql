c8f6d126f6fc70468e631a40ed77b3c7cf17591c
EternalWill <sanerdonery@gmail.com>
2026-01-08 14:28:38 +0200

    add variables

--- db/replications/subscriber/01_sub.sql
+++ db/replications/subscriber/01_sub.sql
@@ -1,3 +1,3 @@
 CREATE SUBSCRIPTION my_sub
-CONNECTION 'host=db port=5432 user=user password=password dbname=traveler_db'
+CONNECTION 'host=${DOCKER_DB_HOST} port=5432 user=${DB_USER} password=${DB_PASSWORD} dbname=traveler_db'
 PUBLICATION my_pub;
